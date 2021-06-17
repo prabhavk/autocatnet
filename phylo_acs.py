5d25973c70ca4379a1a3ba52133e6bb80cbac06e
prabhavk <prabhavk@mpi-inf.mpg.de>
2021-06-17 17:07:15 +0200

    added parser for ACS

--- phylo_acs.py
+++ phylo_acs.py
@@ -10,17 +10,24 @@ class Network:
     def Read_ACS(self, ACS_file_name):
         ACS_file = open(ACS_file_name, "r")
         for line in ACS_file:
-            # Parse line
-            # Let's say the reaction R1 is under consideration
-            self.reactions["R1"] = {}
-            self.reactions["R1"]["in"] = ["A"]
-            self.reactions["R1"]["out"] = ["B"]
-            self.reactions["R1"]["cat"] = ["D"]
-            # Let's say the reaction R2 is under consideration
-            self.reactions["R2"] = {}
-            self.reactions["R2"]["in"] = ["C"]
-            self.reactions["R2"]["out"] = ["D"]
-            self.reactions["R2"]["cat"] = ["B"]
+            (reaction_name, molecules_to_be_parsed) = line.strip().split(":")
+            # print ("reaction_name", reaction_name)
+            # print ("molecules_to_be_parsed", molecules_to_be_parsed)
+            reactants_and_catalysts, products_string = molecules_to_be_parsed.split("->") # change for reversible reactions
+            # print ("reactants_and_catalysts", reactants_and_catalysts)
+            # print ("products_string", products_string)
+            products_list = products_string.split("+")
+            reactants_string, catalysts_string = reactants_and_catalysts.split("[")
+            reactants_list = reactants_string.split("+")            
+            print (catalysts_string)
+            catalysts_list = catalysts_string.split("]")[0].split(",")
+            self.reactions[reaction_name] = {}
+            self.reactions[reaction_name]["in"] = reactants_list
+            self.reactions[reaction_name]["out"] = products_list
+            self.reactions[reaction_name]["cat"] = catalysts_list
+            self.molecules.update(set(reactants_list))
+            self.molecules.update(set(products_list))
+            self.molecules.update(set(catalysts_list))
         ACS_file.close()
     # Hordijk and Steel (2004)
     def RAF(self):
